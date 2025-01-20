5fb9ec95e4c858350059ce1469266ee731d60688
David Rose <davidrose@Davids-MacBook-Air.local>
2025-01-20 16:14:04 -0700

    add steps to copy game feedback

--- src/main.py
+++ src/main.py
@@ -9,10 +9,20 @@ def main():
 
     guess_feedback = []
 
+
     while not game.is_game_over(guess_feedback):
         guess = word_picker.choose_word(guess_feedback) 
         game.enter_guess(guess)
         guess_feedback = game.read_game_feedback()
+
+    # Close popup to sign up for NYT
+    game.page.click("button[aria-label = 'Close']")
+
+    # Copy share text
+    results = game.get_share_button_content()
+
+    # Print the button text
+    print(results)
 
     # Closing the browser when done
     game.browser.close()
